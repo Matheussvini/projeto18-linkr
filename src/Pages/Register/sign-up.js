042b461c45b2f2e68bb0a731a25f4f1044809e45
Pedro Reis <phbarcelosreis66@gmail.com>
2023-01-04 14:07:40 -0300

    feat: refinements and send to api the register

--- src/Pages/Register/sign-up.js
+++ src/Pages/Register/sign-up.js
@@ -1,6 +1,11 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate as navigate } from "react-router-dom";
 import { useState } from "react";
+import {
+    App,
+    Introduction,
+    Register
+} from "./signupStyles.js"
+import axios from "axios"
 
 function SignUp() {
 
@@ -13,6 +18,40 @@ function SignUp() {
 
     /* ----------------------- */
 
+    const api = ""
+
+    function Teste(e) {
+        e.preventDefault();
+        RegisterUser();
+    }
+
+    function RegisterUser() {
+
+        if (password.length < 3) {
+            return alert("A senha é insegura, por favor digite uma senha com mais de 3 caracteres!");
+        }
+
+        alert("teste")
+
+        const userObj = {
+            email,
+            password,
+            username,
+            url
+        }
+
+        const promise = axios.post(userObj, api)
+        promise.then(() => {
+            navigate("/");
+
+        })
+        promise.catch((error) => {
+            console.log(error);
+        })
+
+
+    }
+
     return (
         <App>
             <Introduction>
@@ -24,10 +63,10 @@ function SignUp() {
                     the best links on the web
                 </p>
             </Introduction>
-            <Register>
+            <Register onSubmit={Teste}>
                 <input onChange={(e) => setEmail(e.target.value)} placeholder="e-mail" type="email" required />
                 <input onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password" required />
-                <input onChange={(e) => setUser(e.target.value)} type="text" placeholder="username"  required />
+                <input onChange={(e) => setUser(e.target.value)} type="text" placeholder="username" required />
                 <input onChange={(e) => setPicture(e.target.value)} placeholder="picture url" type="text" required />
                 <button type="submit"><h1>Sign Up</h1></button>
                 <Link to="/"><p>Switch back to log in</p></Link>
@@ -36,69 +75,5 @@ function SignUp() {
     )
 
 }
-
-const App = styled.div`
-    display: flex;
-    width: 100vw;
-    height: 100vh;
-    background-color: #151515;
-`
-
-const Introduction = styled.div`
-    box-sizing: border-box;
-    padding-left: 144px;
-    padding-top: 301px;
-    width: 65%;
-    height: 100vh;
-    background-color: #151515;
-    h1{
-        width: 233px;
-        height: 117px;
-        font-family: 'Passion One';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 106px;
-        line-height: 117px;
-        letter-spacing: 0.05em;
-        color: #FFFFFF;
-    } 
-    
-    p {
-        width: 442px;
-        height: 128px;
-        font-family: 'Oswald';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 43px;
-        line-height: 64px;
-        color: #FFFFFF;
-    }
-`
-
-const Register = styled.form`
-    box-sizing: border-box;
-    padding-top: 274px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 35%;
-    height: 100vh;
-    background-color: #333333;
-    gap: 13px;
-    input{
-        width: 429px;
-        height: 65px;
-        background: #FFFFFF;
-        border-radius: 6px;
-    }
-
-    button{
-        width: 429px;
-        height: 65px;
-        background: #1877F2;
-        border-radius: 6px;
-        border: none;
-    }
-`
 
 export default SignUp;
